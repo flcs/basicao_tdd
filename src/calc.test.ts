ea20fd78203974b929a1ab89a42e1c8ba0407937
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2022-09-20 14:16:42 -0300

    primeiro teste

---
+++ src/calc.test.ts
@@ -0,0 +1,15 @@
+import add from './add';
+
+describe('add' , () => {
+    it('Ele deve somar num1 e num2 e dar num3', () => {
+        // Dado que (setup):
+        const sut = add
+        const expectativa = 3
+
+        // Quando (ação):
+        const retorno = sut(1,2)
+
+        // Então (expectativa):
+        expect(retorno).toEqual(expectativa)
+    })
+})
