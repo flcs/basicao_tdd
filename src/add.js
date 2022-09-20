c9258e38b518a2ec47e05be02a5b2043bcce6124
Fernando Carvalho <fernando.carvalho.iff@gmail.com>
2022-09-20 14:22:05 -0300

    primeiro teste

---
+++ src/add.js
@@ -0,0 +1,7 @@
+function add(num1, num2) {
+    const num3 = num1 + num2
+    return num3
+}
+
+module.exports = add
+// export default add
--- src/calc.test.js
+++ src/calc.test.js
@@ -1,13 +1,13 @@
-import add from './add';
+var add = require('./add');
 
-describe('add' , () => {
+describe('add', () => {
     it('Ele deve somar num1 e num2 e dar num3', () => {
         // Dado que (setup):
         const sut = add
         const expectativa = 3
 
         // Quando (ação):
-        const retorno = sut(1,2)
+        const retorno = sut(1, 2)
 
         // Então (expectativa):
         expect(retorno).toEqual(expectativa)
